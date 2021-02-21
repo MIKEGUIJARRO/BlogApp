6333c789f8147c55c89590e91de08c21fae4674c
Mike Guijarro <a01612042@itesm.mx>
2021-02-21 11:55:57 -0600

    Process.env.PORT added

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.delete("/blogs/:id", function (req, res) {
 });
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server up...");
 });
 
